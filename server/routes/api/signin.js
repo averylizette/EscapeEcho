45e188f92ecb046d938d2612203dfe6ccd932826
Avery <averylisneil@gmail.com>
2019-12-27 14:40:23 -0800

    Finish building API routes for login, logout, verify, and create user

--- server/routes/api/signin.js
+++ server/routes/api/signin.js
@@ -188,6 +188,32 @@ module.exports = (app) => {
 
   });
 
+  app.get('/api/account/logout', (req, res, next) => {
+    const { query } = req;
+    const { token } = query;
+
+    UserSession.findOneAndUpdate({
+        _id: token,
+        isDeleted: false
+    }, {
+        $set: {
+            isDeleted: true}
+
+    }, null, (err, sessions) => {
+        if (err) {
+            return res.send({
+                success: false,
+                message: "Error: Failed to logout of session"
+            })
+        } else {
+            return res.send({
+                success: true,
+                message: "Logged out"
+            })
+        }
+    })
+
+    });
 };
 
 
