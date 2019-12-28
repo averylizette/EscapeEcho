ba32e3e173aecad830262925f7a55b8f5307f2a0
Avery <averylisneil@gmail.com>
2019-12-27 17:37:56 -0800

    Create input fields for user information

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -9,9 +9,23 @@ class App extends Component {
             isLoading: false,
             signUpError: '',
             signInError:'',
-            token: ''
+            token: '',
+            signInEmail: '',
+            signInPassword: '',
+            signUpFirstName: '',
+            signUpLastName: '',
+            signUpError: '',
+            signUpEmail: '',
+            signUpPassword: ''
         };
-        
+        this.onChangeSignInEmail = this.onChangeSignInEmail.bind(this);
+        this.onChangeSignInPassword = this.onChangeSignInPassword.bind(this);
+        this.onChangeSignUpPassword = this.onChangeSignUpPassword.bind(this);
+        this.onChangeSignUpFirstName = this.onChangeSignUpFirstName.bind(this);
+        this.onChangeSignUpLastName = this.onChangeSignUpLastName.bind(this);
+        this.onChangeSignUpEmail = this.onChangeSignUpEmail.bind(this);
+        this.onSignIn = this.onSignIn.bind(this);
+        this.onSignUp = this.onSignUp.bind(this);
     }
 
     componentDidMount() {
@@ -40,12 +54,93 @@ class App extends Component {
         }
     }
     
+    onChangeSignInEmail(event) {
+        this.setState({
+            signInEmail: event.target.value
+        })
+    }
+    onChangeSignUpEmail(event) {
+        this.setState({
+            signUpEmail: event.target.value
+        })
+    }
+    onChangeSignInPassword(event) {
+        this.setState({
+            signInPassword: event.target.value
+        })
+    }
+
+    onChangeSignUpPassword(event) {
+        this.setState({
+            signUpPassword: event.target.value
+        })
+    }
+
+    onChangeSignUpFirstName(event) {
+        this.setState({
+            signUpFirstName: event.target.value
+        })
+    }
+
+    onChangeSignUpLastName(event) {
+        this.setState({
+            signUpLastName: event.target.value
+        })
+    }
+
+    onSignUp() {
+        const {
+            signUpFirstName,
+            signUpLastName,
+            signUpEmail,
+            signUpPassword
+        } = this.state;
+
+        this.setState({
+            isLoading: true
+        })
+
+        fetch('/api/account/signup', {
+            method: 'POST',
+            body: JSON.stringify({
+                firstName: signUpFirstName,
+                lastName: signUpLastName,
+                email: signUpEmail,
+                password: signUpPassword
+            }),
+        }).then(json => {
+            if (json.success) {
+                this.setState({
+                    signUpError: json.message,
+                    isLoading: false,
+                    signUpEmail: '',
+                    signInPassword: '',
+                    signUpFirstName: '',
+                    signUpLastName: ''
+                }) // dont need to do this if I am then redirecting to another page because it will then clear automatically 
+            }
+            
+        })
+
+    }
+
+    onSignIn() {
+
+    }
     
     render() {
         const {
             isLoading,
             token,
-        } = this.state
+            signInError,
+            signInPassword,
+            signUpEmail,
+            signInEmail,
+            signUpFirstName,
+            signUpLastName,
+            signUpError,
+            signUpPassword
+        } = this.state;
 
         if (isLoading) {
             return <div><p>Loading...</p></div>
@@ -55,19 +150,29 @@ class App extends Component {
             return (
                 <div>
                     <div>
+                        {
+                            (signInError) ? (
+                                <p>{signInError}</p>
+                            ) : (null)
+                        }
                        <p>sign in</p>
-                        <input type="email" placeholder="Email"/><br/>
-                        <input type="password" placeholder="Password"/><br/>
-                        <button>Sign In</button>
+                        <input type="email" placeholder="Email" value={signInEmail} onChange={this.onChangeSignInEmail}/><br/>
+                        <input type="password" placeholder="Password" value={signInPassword} onChange={this.onChangeSignInPassword}/><br/>
+                        <button onClick={this.onSignIn}>Sign In</button>
                     </div>
                     <br/>
                     <br/>
                     <p>sign up</p>
-                    <input type="text" placeholder="First Name"/><br/>
-                    <input type="text" placeholder="Last Name"/><br/>
-                    <input type="email" placeholder="Email"/><br/>
-                    <input type="password" placeholder="Password"/><br/>
-                    <button>Sign Up</button>
+                    {
+                            (signUpError) ? (
+                                <p>{signUpError}</p>
+                            ) : (null)
+                        }
+                    <input type="text" placeholder="First Name" value={signUpFirstName} onChange={this.onChangeSignUpFirstName}/><br/>
+                    <input type="text" placeholder="Last Name" value={signUpLastName} onChange={this.onChangeSignUpLastName}/><br/>
+                    <input type="email" placeholder="Email" value={signUpEmail} onChange={this.onChangeSignUpEmail}/><br/>
+                    <input type="password" placeholder="Password" value={signUpPassword} onChange={this.onChangeSignUpPassword}/><br/>
+                    <button onClick={this.onSignUp}>Sign Up</button>
                 </div>
             )
         }
